d2c5743c7ab98935d6e57112191aed55d94c3037
Crystian Lefundes <crystian.lf@gmail.com>
2021-08-24 03:34:15 -0300

    Create the screenImage component

---
+++ src/components/screensImage/index.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const ScreensImage = styled.div`
+    width: 100%;
+    max-height: 38rem;
+    margin-top: 4rem;
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+    background: linear-gradient(180deg, rgba(30, 30, 30, 0) 0%, rgba(14, 14, 14, .8) 100%);
+
+    img {
+        min-width: 55rem;
+        height: 100%;
+        z-index: -1;
+    }
+`
