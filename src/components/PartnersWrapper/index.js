91dd7471ed4d7cc6d9c02b5e5efcb1ec8915fafd
Crystian Lefundes <crystian.lf@gmail.com>
2021-08-25 00:01:50 -0300

    Create a PartnersWrapper component

---
+++ src/components/PartnersWrapper/index.js
@@ -0,0 +1,26 @@
+import * as S from './styles'
+import DefaultButton from '../DefaultButton'
+import logoAppStore from '../../assets/images/logo_app_store.svg'
+import logoApiary from '../../assets/images/logo_apiary.svg'
+import logoAndroid from '../../assets/images/logo_android.svg'
+import logoBaseCamp from '../../assets/images/logo_basecamp.svg'
+import logoAirBnb from '../../assets/images/logo_airbnb.svg'
+import logoIBM from '../../assets/images/logo_ibm.svg'
+
+export default function PartnersWrapper() {
+    return(
+        <S.Container>
+          <S.LogoWrapper>
+            <img src={logoAppStore} alt="Logo da App Store" />
+            <img src={logoApiary} alt="Logo da Apiary" />
+            <img src={logoAndroid} alt="Logo do Android" />
+          </S.LogoWrapper>
+          <S.LogoWrapper>
+            <img src={logoBaseCamp} alt="Logo do Basecamp" />
+            <img src={logoAirBnb} alt="Logo do AirBnb" />
+            <img src={logoIBM} alt="Logo da IBM" />
+          </S.LogoWrapper>
+          <DefaultButton content="All Partners" link="#" />
+        </S.Container>
+    )
+}
---
+++ src/components/PartnersWrapper/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    button {
+        margin-bottom: 15.1rem;
+    }
+`
+
+export const LogoWrapper = styled.div`
+    display: flex;
+    margin: 5rem auto;
+    
+    img {
+        width: 4rem;
+        height: 4rem;
+        margin: 0 1.4rem;
+    }
+`
