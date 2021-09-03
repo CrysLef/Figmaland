955d1c0286ff2e2a78a97eccf8b2cbb6011d7bf6
Crystian Lefundes <crystian.lf@gmail.com>
2021-09-02 23:51:31 -0300

    Modify the gallery for desktop

--- src/components/GalleryWrapper/index.js
+++ src/components/GalleryWrapper/index.js
@@ -1,17 +1,20 @@
 import * as S from './styles'
-import drone from '../../assets/images/cardImage1.svg'
-import manTipying from '../../assets/images/cardImage2.svg'
-import phone from '../../assets/images/cardImage3.svg'
-import pcClosing from '../../assets/images/cardImage4.svg'
 import DefaultButton from '../DefaultButton'
 
 export default function GalleryWrapper() {
     return(
         <S.Container>
-            <S.Image src={drone} alt="" />
-            <S.Image src={manTipying} alt="" />
-            <S.Image src={phone} alt="" />
-            <S.Image src={pcClosing} alt="" />
+            <div>
+                <S.Image className="imgID1" />
+                <S.Image className="imgID2" />
+                <S.Image className="imgID3" />
+                <S.Image className="imgID4" />
+            </div>
+            <div>
+                <S.Image className="imgID5" />
+                <S.Image className="imgID6" />
+                <S.Image className="imgID7" />
+            </div>
             <DefaultButton content="See more" link="#" />
         </S.Container>
     )
--- src/components/GalleryWrapper/styles.js
+++ src/components/GalleryWrapper/styles.js
@@ -1,4 +1,11 @@
 import styled from "styled-components";
+import drone from '../../assets/images/cardImage1.svg'
+import manTipying from '../../assets/images/cardImage2.svg'
+import phone from '../../assets/images/cardImage3.svg'
+import pcClosing from '../../assets/images/cardImage4.svg'
+import codeImage from '../../assets/images/codeImage.svg'
+import phone2 from '../../assets/images/phone2.svg'
+import womanTyping from '../../assets/images/womanTyping.svg'
 
 export const Container = styled.div`
     display: flex;
@@ -6,20 +13,75 @@ export const Container = styled.div`
     align-items: center;
 
     button {
+        margin-top: 4.4rem;
         margin-bottom: 15.1rem;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        background-repeat: no-repeat;
+
+        .imgID1 {
+            background-image: url(${drone});
+        }
+        .imgID2 {
+            background-image: url(${phone});
+        }
+        .imgID3 {
+            background-image: url(${manTipying});
+        }
+        .imgID4 {
+            background-image: url(${pcClosing});
+        }
+        .imgID5 {
+           display: none;
+        }
+        .imgID6 {
+            display: none;
+        }
+        .imgID7 {
+            display: none;
+        }
+
+        @media (min-width: 1020px) {
+            flex-direction: row;
+            margin-right: -10rem;
+
+            .imgID1{
+                margin-left: -12rem;
+            }
+            .imgID2{}
+            .imgID3{}
+            .imgID4{}
+
+            .imgID5 {
+                display: block;
+                background-image: url(${codeImage});
+                margin-right: 2rem;
+                margin-left: -15rem;
+            }
+            .imgID6 {
+                display: block;
+                background-image: url(${womanTyping});
+                margin-right: -14rem;
+            }
+            .imgID7 {
+                display: block;
+                background-image: url(${phone2});
+            }
+        }
     }
 
 `
 
-export const Image = styled.img`
-    width: 26rem;
+export const Image = styled.div`
+    width: 24rem;
     height: 22rem;
+    background-size: contain;
 
-    &:nth-child(1) {
-        margin-top: 4.4rem;
-    }
-
-    &:last-child {
-        margin-bottom: 4.4rem;
+    @media (min-width: 1020px) {
+        width: 38rem;
+        height: 26rem;
     }
 `
