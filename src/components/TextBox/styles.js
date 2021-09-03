d0e3638cdfb5b23bb4a6bb65214788eb570c9363
Crystian Lefundes <crystian.lf@gmail.com>
2021-09-02 23:53:54 -0300

    Fix the TextBox component

--- src/components/TextBox/styles.js
+++ src/components/TextBox/styles.js
@@ -9,16 +9,29 @@ export const Box = styled.div`
         margin: 5.7rem 22rem 0 22rem;
     }
 
+    @media (min-width: 1024px) {
+        margin: 2rem auto;
+        width: 45rem;
+    }
+
     h2 {
         font-weight: 500;
         text-align: center;
         font-size: 3.2rem;
         margin-bottom: 1.7rem;
+
+        @media (min-width: 1024px) {
+            font-size: 4.5rem;
+        }
     }
 
     p {
         text-align: center;
         line-height: 2.5rem;
         font-size: 1.1rem;
+
+        @media (min-width: 1024px) {
+            font-size: 1.6rem;
+        }
     }
 `
