555ddfb2e8acd37089ddab65dac96a48073072ae
Crystian Lefundes <crystian.lf@gmail.com>
2021-09-02 23:52:50 -0300

    Modify the header for desktop

--- src/components/MainHeader/styles.js
+++ src/components/MainHeader/styles.js
@@ -6,18 +6,37 @@ export const Header = styled.header`
     align-items: center;
     justify-content: space-between;
     overflow: hidden;
+
+    @media (min-width: 1020px) {
+        justify-content: center;
+        margin-bottom: 8rem;
+    }
+
 `
 
 export const ButtonHeader = styled(Button)`
     padding: 1.2rem 3.2rem;
+
+    &:hover {
+        background-color: #fff;
+        color: #000;
+    }
 `
 
 export const Logo = styled.img`
     width: 18.6rem;
     height: 5.8rem;
+    margin-left: .8rem;
+    cursor: pointer;
 `
 
 export const WrapperMenu = styled.div`
     display: flex;
     align-items: center;
+    
+
+    @media (min-width: 1020px) {
+        flex-direction: row-reverse;
+        margin-right: 2rem;
+    }
 `
